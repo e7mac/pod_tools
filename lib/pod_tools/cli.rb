fdf45ef8a163fe031dd421d6ffadd10cf57490b0
Mayank Sanganeria <mayank.ot@gmail.com>
2015-03-10 16:29:52 -0700

    add exception handling

--- lib/pod_tools/cli.rb
+++ lib/pod_tools/cli.rb
@@ -10,36 +10,42 @@ class PodTools::CLI < Thor
   method_option :branch, :default => "development",
                 :banner => "branch to switch to before running pod install"
   desc "install", "Install pods from alternative pod paths."
-  def install 
-    say "Copying the original Podfile"
-    FileUtils.cp('Podfile', 'Podfile.original')
+  def install
+    begin 
+      say "Copying the original Podfile"
+      FileUtils.cp('Podfile', 'Podfile.original')
 
-    local_pods = options[:keep]
-    branch = options[:branch]
-    new_lines = read_podfile('Podfile.original', local_pods, branch) do |podname, action| 
-      if (action == :clone) 
-        clone_command = "git clone #{ENV['GIT_REPO_BASE_URL']}/#{podname}.git ../#{podname}"
-        say "Cloning: #{clone_command}"
-        system clone_command
+      local_pods = options[:keep]
+      branch = options[:branch]
+      new_lines = read_podfile('Podfile.original', local_pods, branch) do |podname, action| 
+        if (action == :clone) 
+          clone_command = "git clone #{ENV['GIT_REPO_BASE_URL']}/#{podname}.git ../#{podname}"
+          say "Cloning: #{clone_command}"
+          system clone_command
 
-        checkout_command = "pushd ../#{podname}; git checkout #{branch}; popd"
-        say "Checking out: #{checkout_command}"
-        system checkout_command
+          checkout_command = "pushd ../#{podname}; git checkout #{branch}; popd"
+          say "Checking out: #{checkout_command}"
+          system checkout_command
+        end
       end
+      say "\n"
+      say "Pod installing the below modified Podfile: \n"
+      say "===================================== \n"
+      say "\n"
+      say new_lines
+      IO.write('Podfile', new_lines)
+
+      args = ['install', '--no-repo-update']
+      Pod::Command.run(args)
+
+      say "Copying back the original Podfile."
+      FileUtils.cp('Podfile.original', 'Podfile')
+      FileUtils.rm('Podfile.original')
+    rescue
+      say "Copying back the original Podfile."
+      FileUtils.cp('Podfile.original', 'Podfile')
+      FileUtils.rm('Podfile.original')
     end
-    say "\n"
-    say "Pod installing the below modified Podfile: \n"
-    say "===================================== \n"
-    say "\n"
-    say new_lines
-    IO.write('Podfile', new_lines)
-
-    args = ['install', '--no-repo-update']
-    Pod::Command.run(args)
-
-    say "Copying back the original Podfile."
-    FileUtils.cp('Podfile.original', 'Podfile')
-    FileUtils.rm('Podfile.original')
   end
   
   desc "update", "Update pods from alternative pod paths."
@@ -74,11 +80,7 @@ class PodTools::CLI < Thor
       say "Copying back the original Podfile."
       FileUtils.cp('Podfile.original', 'Podfile')
       FileUtils.rm('Podfile.original')
-    rescue Interrupt
-      say "Copying back the original Podfile."
-      FileUtils.cp('Podfile.original', 'Podfile')
-      FileUtils.rm('Podfile.original')
-    rescue Exception => e
+    rescue 
       say "Copying back the original Podfile."
       FileUtils.cp('Podfile.original', 'Podfile')
       FileUtils.rm('Podfile.original')
